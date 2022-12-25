450b008e10989c20c8b84176fb52b5c20d17a675
Adriana Pacholczyk <ada@MacBook-Pro-Adriana.local>
2022-12-25 11:13:44 +0100

    initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+                .onAppear {
+                    UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+                }
+        }
+    }
+}
